e7e685c63bd477725bc40ad0621d858a2bdf1e92
antony-calderon <alex_aact@hotmail.com>
2023-02-12 14:41:39 -0500

    Actualizando listas

---
+++ curso python/Listas/debanadoListas.py
@@ -0,0 +1,6 @@
+lista = ["Antony" , "Jhony" , 33 , 32 , "casa" , "Miah"]
+
+print(lista[0 : 4])
+print(lista[4 : ])
+print(lista[-3 : ])
+print(lista[-5 : -2])
---
+++ curso python/Listas/listas.py
@@ -0,0 +1,8 @@
+lista = ["antony" , "Jhony", 32 , 31]
+
+print(lista)
+print(len(lista))
+
+lista[0] = "Antony"
+
+print(lista)
