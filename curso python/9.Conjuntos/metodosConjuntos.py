f14e86aa4ae3b9f05bf8ad8df0dfdd7f19d5c6bf
antony-calderon <alex_aact@hotmail.com>
2023-02-13 22:17:14 -0500

    cuarto commit

---
+++ curso python/9.Conjuntos/metodosConjuntos.py
@@ -0,0 +1,17 @@
+conjunto = {1 , 2 , 3 , 4 , 5}
+print(conjunto)
+
+conjunto.add(20)
+print(conjunto)
+
+conjunto.remove(1)
+print(conjunto)
+
+conjunto.pop()
+print(conjunto)
+
+conjunto.update({1 , 6 , 9})
+print(conjunto)
+
+conjunto.clear()
+print (conjunto)
